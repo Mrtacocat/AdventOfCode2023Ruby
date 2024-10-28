678b5ebbfae572d91b6425c37fcd31a922cf0838
mrtacocat <christoffer@riis.cc>
2024-10-28 10:57:49 +0100

    add day 1 & 2

---
+++ day1/day1.rb
@@ -0,0 +1,33 @@
+WORD_TO_DIGIT = {
+  'one' => '1', 'two' => '2', 'three' => '3', 'four' => '4', 'five' => '5',
+  'six' => '6', 'seven' => '7', 'eight' => '8', 'nine' => '9'
+}
+
+def convert_digit_words_to_digit_number(str)
+  str.gsub(/(one|two|three|four|five|six|seven|eight|nine)/i) do |match|
+    WORD_TO_DIGIT[match.downcase]
+  end
+
+end
+
+def extracted_digit_from_string(str)
+  converted_str = convert_digit_words_to_digit_number(str)
+
+  digits = converted_str.scan(/\d/)
+  first_digit = digits.first
+  last_digit  = digits.last
+
+  if first_digit && last_digit
+    (first_digit + last_digit).to_i
+  else
+    0
+  end
+end
+
+sum = 0
+
+File.foreach('input.txt') do |line|
+  sum += extracted_digit_from_string(line.strip)
+end
+
+puts "The total is #{sum}"
---
+++ day2/day2.rb
@@ -0,0 +1,39 @@
+# Maximum allowed cubes for each color
+MAX_RED = 12
+MAX_GREEN = 13
+MAX_BLUE = 14
+
+# Check if a game is possible based on the cube counts in each set
+def game_possible?(sets)
+  sets.each do |set|
+
+    # Parse each color count (e.g., "3 blue")
+    set.split(', ').each do |color_count|
+      count, color = color_count.split
+      count = count.to_i
+
+      # Check if this set violates any color limit
+      return false if (color == "red" && count > MAX_RED) ||
+        (color == "green" && count > MAX_GREEN) ||
+        (color == "blue" && count > MAX_BLUE)
+    end
+  end
+
+  # If no violations, the game is possible
+  true
+end
+
+# Read the input file and process games
+possible_game_ids_sum = 0
+
+File.foreach("day2/input.txt") do |line|
+  if line =~ /Game (\d+): (.+)/
+    game_id = $1.to_i
+    sets = $2.split('; ')
+
+    # Check if the game is possible and add its ID to the sum if so
+    possible_game_ids_sum += game_id if game_possible?(sets)
+  end
+end
+
+puts "Sum of possible game IDs: #{possible_game_ids_sum}"
